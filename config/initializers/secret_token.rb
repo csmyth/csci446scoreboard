94874f18a0d36a703f626647e228ad436e09faf3
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-05-09 21:27:24 -0600

    Intial commit of Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = '76261d8b307364341bf03c2341beb3dfef34c4e411908ec6b1095dc513938b3f6ba7c7349c0307249012963b0e51ab905436edbe2479acf0f2d687d0b2ca6252'
